760462993d59a45bf47ff64ff05b01a4cf2188fc
unmoein <unmoein@gmail.com>
2025-01-03 16:44:00 +0330

    fixing relations

--- Abriapp/news_app/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsSuperUser(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.role == 'SUPER_USER'
-
-class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.role == 'ADMIN'
