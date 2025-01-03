bde1192a82f40261b1050b88730d3cff8a06d320
unmoein <unmoein@gmail.com>
2025-01-03 14:33:20 +0330

    Update settings.py

--- Abriapp/Abriapp/settings.py
+++ Abriapp/Abriapp/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = "Abriapp.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'news_db',
+        'USER': 'unmoein',
+        'PASSWORD': 'moein123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
