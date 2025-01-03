2417ba9c67a18b220f9e748c30cccfc41ad26f86
unmoein <unmoein@gmail.com>
2025-01-03 19:23:32 +0330

    database

--- contactform/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContactformConfig(AppConfig):
-    name = 'contactform'
--- newsletter/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Newsletter
-# Register your models here.
-
-admin.site.register(Newsletter)
